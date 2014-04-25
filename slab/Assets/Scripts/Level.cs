4568cdde3245e117acc1548c018fb3733cefadd2
Brian Zhang <Brian@Brians-MacBook-Pro.local>
2014-04-25 01:42:44 -0400

    generate the walls in a separate function

--- slab/Assets/Scripts/Level.cs
+++ slab/Assets/Scripts/Level.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class Level : MonoBehaviour {
-
 	public int levelWidth = 3;
 	public int levelHeight = 3;
+	public int levelFill = 1;
 
 	public int tileWidth = 128;
 	public int tileHeight = 128;
@@ -13,42 +13,53 @@ public class Level : MonoBehaviour {
 	
 	private int[,] levelTiles;
 	private GameObject[,] tiles;
-
-	private static int ROW = 0;
-	private static int COL = 1;
+	private const int EMPTY_TILE = 0;
+	private const int FILLED_TILE = 1;
 
 	// Use this for initialization
-	void Start () {
-
+	void Start() {
 		levelTiles = new int[levelWidth, levelHeight];
+		levelTiles = TemplateLevel();
 		tiles = new GameObject[levelWidth, levelHeight];
-		for (int row = 0; row < levelTiles.GetLength(ROW); row++) {
-			for(int col = 0; col < levelTiles.GetLength(COL); col++){
-
-
-
-				levelTiles[row, col] = Random.Range(0, 11);
-				if( levelTiles[row, col] == 1 || 
-				   	col == 0 || 
-				   	col + 1 == levelTiles.GetLength(COL) || 
-					row == 0 ||
-				   	row + 1 == levelTiles.GetLength(ROW))
-				{
+		for (int row = 0; row < levelWidth; row++) {
+			for(int col = 0; col < levelHeight; col++){
+				if(levelTiles[row, col] != FILLED_TILE) {
+					levelTiles[row, col] = randomFillTile ();
+				}
+				if(levelTiles[row, col] == 1) {
 					tiles[row, col] = (GameObject)Instantiate(tileSprite, new Vector3(row, col, 1), Quaternion.identity);
 				}
 				//Debug.Log(levelTiles[row, col]);
 			}
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-
+	int[,] TemplateLevel() {
+		int[,] levelTemplate = new int[levelWidth, levelHeight];
+		for (int row = 0; row < levelWidth; row++) {
+			levelTemplate[row, 0] = FILLED_TILE;				// left wall
+			levelTemplate[row, levelHeight-1] = FILLED_TILE;	// right wall
+		}
+		for (int col = 0; col < levelHeight; col++) {
+			levelTemplate [0, col] = FILLED_TILE;				// ceiling
+			levelTemplate [levelWidth - 1, col] = FILLED_TILE;	// floor
+		}
+		return levelTemplate;
 	}
 
-	void OnGUI(){
+	int randomFillTile() {
+		int fill = 0;
+		int random = Random.Range (0, 10);
+		if (random < levelFill) {
+			fill = 1;
+		}
+		return fill;
+	}
 
+	// Update is called once per frame
+	void Update() {
+	}
 
+	void OnGUI() {
 	}
 }
--- slab/Assets/Scripts/PlayerControl.cs
+++ slab/Assets/Scripts/PlayerControl.cs
@@ -8,7 +8,6 @@ public class PlayerControl : MonoBehaviour
 	[HideInInspector]
 	public bool jump = false;				// Condition for whether the player should jump.
 
-
 	public float moveForce = 365f;			// Amount of force added to move the player left and right.
 	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
 	public AudioClip[] jumpClips;			// Array of clips for when the player jumps.
@@ -17,8 +16,7 @@ public class PlayerControl : MonoBehaviour
 	private Transform groundCheck;			// A position marking where to check if the player is grounded.
 	private bool grounded = false;			// Whether or not the player is grounded
 	//private Animator anim;					// Reference to the player's animator component.
-
-
+	
 	void Awake()
 	{
 		// Setting up references.
@@ -26,26 +24,18 @@ public class PlayerControl : MonoBehaviour
 		//anim = GetComponent<Animator>();
 	}
 
-
 	void Update()
 	{
 		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
 		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")); 
 
 		// If the jump button is pressed and the player is grounded then the player should jump.
-		if(Input.GetButtonDown("Jump") && grounded)
+		if(Input.GetButtonDown("Jump") && grounded) {
 			jump = true;
-
-
-
+		}
 	}
-
 	
 	void FixedUpdate (){
-
-
-
-
 		// Cache the horizontal input.
 		float h = Input.GetAxis("Horizontal");
 
@@ -53,24 +43,25 @@ public class PlayerControl : MonoBehaviour
 		//anim.SetFloat("Speed", Mathf.Abs(h));
 
 		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-		if(h * rigidbody2D.velocity.x < maxSpeed)
+		if(h * rigidbody2D.velocity.x < maxSpeed) {
 			// ... add a force to the player
 			rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		}
 
 		// If the player's horizontal velocity is greater than the maxSpeed...
-		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed) {
 			// ... set the player's velocity to the maxSpeed in the x axis.
 			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		}
 
 		// If the input is moving the player right and the player is facing left...
-		if(h > 0 && !facingRight)
-			// ... flip the player.
-			Flip();
+		if(h > 0 && !facingRight) {
+			FlipPlayer();
+		}
 		// Otherwise if the input is moving the player left and the player is facing right...
-		else if(h < 0 && facingRight)
-			// ... flip the player.
-			Flip();
-
+		else if(h < 0 && facingRight) {
+			FlipPlayer();
+		}
 
 		// If the player should jump...
 		if(jump) {
@@ -88,9 +79,8 @@ public class PlayerControl : MonoBehaviour
 			jump = false;
 		}
 	}
-	
-	
-	void Flip ()
+
+	void FlipPlayer ()
 	{
 		// Switch the way the player is labelled as facing.
 		facingRight = !facingRight;
@@ -100,6 +90,4 @@ public class PlayerControl : MonoBehaviour
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
-
-
 }
