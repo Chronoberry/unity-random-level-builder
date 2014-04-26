5057751fc1e8de11bc984ce906fb57d8b34c6104
Justin Lindsey <jlindsey@chronobit.com>
2014-04-26 14:18:52 -0400

    some silly background images

--- slab/Assets/Scripts/PlayerControl.cs
+++ slab/Assets/Scripts/PlayerControl.cs
@@ -30,32 +30,32 @@ public class PlayerControl : MonoBehaviour
 
             // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
             if(h * rigidbody2D.velocity.x < maxSpeed) {
-                    // ... add a force to the player
-                    rigidbody2D.AddForce(Vector2.right * h * moveForce);
+                // ... add a force to the player
+                rigidbody2D.AddForce(Vector2.right * h * moveForce);
             }
 
             if(v * rigidbody2D.velocity.y < maxSpeed) {
-                    // ... add a force to the player
-                    rigidbody2D.AddForce(Vector2.up * v * moveForce);
+                // ... add a force to the player
+                rigidbody2D.AddForce(Vector2.up * v * moveForce);
             }   
 
             // If the player's horizontal velocity is greater than the maxSpeed...
             if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed) {
-                    // ... set the player's velocity to the maxSpeed in the x axis.
-                    rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+                // ... set the player's velocity to the maxSpeed in the x axis.
+                rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
             }
 
             if(Mathf.Abs(rigidbody2D.velocity.y) > maxSpeed) {
-                    // ... set the player's velocity to the maxSpeed in the x axis.
-                    rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, Mathf.Sign(rigidbody2D.velocity.y) * maxSpeed);
+                // ... set the player's velocity to the maxSpeed in the x axis.
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, Mathf.Sign(rigidbody2D.velocity.y) * maxSpeed);
             }
             // If the input is moving the player right and the player is facing left...
             if(h > 0 && !facingRight) {
-                    FlipPlayer();
+                FlipPlayer();
             }
             // Otherwise if the input is moving the player left and the player is facing right...
             else if(h < 0 && facingRight) {
-                    FlipPlayer();
+                FlipPlayer();
             }
 
 	}
