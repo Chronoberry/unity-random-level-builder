39d66cdc80f080a097b38dddb62c9d07bba209c3
Brian Zhang <tangerinewhite32@gmail.com>
2014-04-27 00:34:49 -0400

    redistributed treasure chests

--- slab/Assets/Scripts/Level.cs
+++ slab/Assets/Scripts/Level.cs
@@ -10,15 +10,17 @@ public class Level : MonoBehaviour {
     public int tileHeight = 128;
 
     public GameObject tileSprite;
+	public GameObject transparentSprite;
     public GameObject boat; 
     public GameObject treasureChest; 
-    public int MAX_CHESTS = 4;
+    public int maxChests = 4;
 
     private int currentChests = 0;
     private int[,] levelTiles;
     private GameObject[,] tiles;
     private const int EMPTY_TILE = 0;
     private const int FILLED_TILE = 1;
+	private const int TRANSPARENT_TILE = 2;
 
 
     void Start() {
@@ -30,16 +32,19 @@ public class Level : MonoBehaviour {
         levelTiles = new int[levelWidth, levelHeight];
         levelTiles = TemplateLevel();
         tiles = new GameObject[levelWidth, levelHeight];
-        for (int row = 0; row < levelWidth; row++) {
-            for(int col = 0; col < levelHeight; col++){
-                if(levelTiles[row, col] != FILLED_TILE) {
+        for (int col = 0; col < levelHeight; col++) {
+            for(int row = 0; row < levelWidth; row++){
+				if(levelTiles[row, col] != FILLED_TILE && levelTiles[row, col] != TRANSPARENT_TILE) {
                     levelTiles[row, col] = randomFillTile ();
                 }
-                if(levelTiles[row, col] == 1) {
+				if(levelTiles[row, col] == FILLED_TILE) {
                     tiles[row, col] = (GameObject)Instantiate(tileSprite, new Vector3(row, col, 1), Quaternion.identity);
                 }
-				if(randomFillChest() == 1 && currentChests < MAX_CHESTS && col !=0 && levelTiles[row,col-1] != 1) {
-			        treasureChest = (GameObject)Instantiate( treasureChest, new Vector3(row,col-1,0f), Quaternion.identity);
+				else if(levelTiles[row, col] == TRANSPARENT_TILE) {
+					tiles[row, col] = (GameObject)Instantiate(transparentSprite, new Vector3(row, col, 1), Quaternion.identity);
+				}
+				if(randomFillChest() == 1 && currentChests < maxChests && col !=0 && levelTiles[row, col+1] != FILLED_TILE) {
+					treasureChest = (GameObject)Instantiate(treasureChest, new Vector3(row, col+1, 1), Quaternion.identity);
 					currentChests += 1;
 				}
             }
@@ -48,20 +53,20 @@ public class Level : MonoBehaviour {
 
     int[,] TemplateLevel() {
         int[,] levelTemplate = new int[levelWidth, levelHeight];
-        for (int row = 0; row < levelWidth; row++) {
-            levelTemplate[row, 0] = FILLED_TILE;				// left wall
-            levelTemplate[row, levelHeight-1] = FILLED_TILE;	// right wall
-        }
-        for (int col = 0; col < levelHeight; col++) {
-            levelTemplate [0, col] = FILLED_TILE;				// ceiling
-            levelTemplate [levelWidth - 1, col] = FILLED_TILE;	// floor
+		for (int col = 0; col < levelHeight; col++) {
+			levelTemplate [0, col] = FILLED_TILE;					// left wall
+			levelTemplate [levelWidth - 1, col] = FILLED_TILE;		// right wall
+		}
+		for (int row = 0; row < levelWidth; row++) {
+			levelTemplate[row, 0] = FILLED_TILE;					// floor
+			levelTemplate[row, levelHeight-1] = TRANSPARENT_TILE;	// ceiling
         }
         return levelTemplate;
     }
 
     int randomFillTile() {
         int fill = 0;
-        int random = Random.Range (0, 13);
+        int random = Random.Range(0, 13);
         if (random < levelFill) {
                 fill = 1;
         }
@@ -70,8 +75,8 @@ public class Level : MonoBehaviour {
 
 	int randomFillChest() {
 		int fill = 0;
-		int random = Random.Range (1, 11);
-		if (random < 2) {
+		int random = Random.Range(0, 20);
+		if (random < 1) {
 			fill = 1;
 		}
 		return fill;
